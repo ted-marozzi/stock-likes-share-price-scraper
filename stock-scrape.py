3b067791cf19cef4d7c163d6c37f32ab2f5d3bec
Ted Marozzi <ted.marozzi@iress.com>
2021-06-05 16:33:51 +1000

    a

--- stock-scrape.py
+++ stock-scrape.py
@@ -8,7 +8,6 @@ for i in range(5):
 
     try:
         pageName = "pointsbet"
-        log._writeSoupToFile(fb.getPageSoup(pageName), pageName)
         likes  = fb.getPageLikes(pageName, fb.getPageSoup(pageName))
         sharePrice = asx.getSharePrice(asx.getAsxSoup("PBH"))
         log.log(pageName, likes, sharePrice)

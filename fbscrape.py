7c2074dd40c1bba4321f265fbfc9add641b2aa57
Ted Marozzi <ted.marozzi@iress.com>
2021-06-05 16:45:05 +1000

    sa

--- fbscrape.py
+++ fbscrape.py
@@ -80,7 +80,7 @@ def getPageLikes(pageName, pageSoup):
         indexNumLikes = 0
 
         # Extract number of page likes
-        numberOfLikesArr = pageSoup.find_all(elementToScrape)
+        numberOfLikesArr = pageSoup.find_all(elementToScrape, class_=classNumLikes)
 
         numbers = []
         print("Potential numbers:", numberOfLikesArr)

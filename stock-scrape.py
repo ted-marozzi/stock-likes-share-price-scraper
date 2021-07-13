192cec4c679ed75eb45884c21957c77eb3409e5d
Lucas Shugg <lucaspshugg@gmail.com>
2021-07-13 17:07:48 +1000

    Test

--- stock-scrape.py
+++ stock-scrape.py
@@ -6,26 +6,26 @@ import log
 for i in range(5):
 
     try:
-        # pageName = "pointsbet"
-        # likes  = fb.getPageLikes(pageName, fb.getPageSoup(pageName))
-        # sharePrice = asx.getSharePrice(asx.getAsxSoup("PBH"))
-        # log.log(pageName, likes, sharePrice)
+        pageName = "pointsbet"
+        likes  = fb.getPageLikes(pageName, fb.getPageSoup(pageName))
+        sharePrice = asx.getSharePrice(asx.getAsxSoup("PBH"))
+        log.log(pageName, likes, sharePrice)
 
-        # pageName = "ZipAU"
-        # likes  = fb.getPageLikes(pageName, fb.getPageSoup(pageName))
-        # sharePrice = asx.getSharePrice(asx.getAsxSoup("Z1P"))
-        # log.log(pageName, likes, sharePrice)
+        pageName = "ZipAU"
+        likes  = fb.getPageLikes(pageName, fb.getPageSoup(pageName))
+        sharePrice = asx.getSharePrice(asx.getAsxSoup("Z1P"))
+        log.log(pageName, likes, sharePrice)
 
-        # pageName = "mineralresourceslimited"
-        # likes  = fb.getPageLikes(pageName, fb.getPageSoup(pageName))
-        # sharePrice = asx.getSharePrice(asx.getAsxSoup("MIN"))
-        # log.log(pageName, likes, sharePrice)
+        pageName = "mineralresourceslimited"
+        likes  = fb.getPageLikes(pageName, fb.getPageSoup(pageName))
+        sharePrice = asx.getSharePrice(asx.getAsxSoup("MIN"))
+        log.log(pageName, likes, sharePrice)
 
 
-        # pageName = "afterpay.it"
-        # likes  = fb.getPageLikes(pageName, fb.getPageSoup(pageName))
-        # sharePrice = asx.getSharePrice(asx.getAsxSoup("APT"))
-        # log.log(pageName, likes, sharePrice)
+        pageName = "afterpay.it"
+        likes  = fb.getPageLikes(pageName, fb.getPageSoup(pageName))
+        sharePrice = asx.getSharePrice(asx.getAsxSoup("APT"))
+        log.log(pageName, likes, sharePrice)
         
         pageName = "TESLAOfficialPage"
         likes  = fb.getPageLikes(pageName, fb.getPageSoup(pageName))
